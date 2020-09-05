b75f8ce489c46fedc845b1bc59f337ad095d28dd
Piotr Ciesiolkiewicz <piotr@displaysweet.com>
2020-09-05 16:09:32 +1000

    Debounce added to the bpm slider

--- src/components/BpmController/BpmController.js
+++ src/components/BpmController/BpmController.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { setBpm } from '@features/metronome/metronome.slice';
 import { selectBpm } from '@features/metronome/metronome.slice';
 import { Box, Slider, InputLabel } from '@material-ui/core';
+import debounce from 'lodash.debounce';
+
+const SET_BPM_DEBOUNCE_WAIT = 100;
 
 function BpmController() {
     const dispatch = useDispatch();
-    const handleBpmChange = (_, value) => dispatch(setBpm(value));
     const bpm = useSelector(selectBpm);
+    const [localBpm, setLocalBpm] = useState(bpm);
+    const debouncedSetBpm = useCallback(debounce(bpm => dispatch(setBpm(bpm)), SET_BPM_DEBOUNCE_WAIT));
+    const handleBpmChange = (_, value) => {
+        setLocalBpm(value);
+        debouncedSetBpm(value);
+    };
 
     return (
         <Box>
             <InputLabel id="bpm-slider-label">BPM</InputLabel>
             <Slider
-                value={bpm}
+                value={localBpm}
                 onChange={handleBpmChange}
                 aria-labelledby="bpm-slider-label"
                 valueLabelDisplay="auto"
